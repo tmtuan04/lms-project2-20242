88f30a56ac80f8748666a0b5876921da82e46552
tuanvdtd <chuhoi009.dt@gmail.com>
2025-03-26 11:40:15 +0700

    add dasboard fe

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import SideNav from '@/app/(dashboard)/_components/Sidebar';
+// import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+                <div className="w-full flex-none md:w-64">
+                    <SideNav />
+                </div>
+                <div className="flex-grow  md:overflow-y-auto scrollbar-none  ">
+                    {children}
+                </div>
+            </div>
+        </>
+    );
+}
