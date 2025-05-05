211be518566e5067e71a0b5293a3c30bd17826e3
tuanvdtd <chuhoi009.dt@gmail.com>
2025-05-05 22:01:07 +0700

    add UI lock chapter

--- app/[courseId]/components/MarkButton.tsx
+++ app/[courseId]/components/MarkButton.tsx
@@ -17,7 +17,7 @@ const MarkButton: React.FC<MarkButtonProps> = ({ initialMarked }) => {
     return (
         <button
             onClick={toggleMarked}
-            className={`flex items-center gap-2 px-4  rounded-md cursor-pointer py-[6px] text-white ${isMarked ? "bg-[#0fba82] hover:bg-[#04976a]" : " bg-blue-600 hover:bg-blue-700"
+            className={`flex items-center gap-2 px-4  rounded-md cursor-pointer py-[5px] text-white ${isMarked ? "bg-[#0fba82] hover:bg-[#04976a]" : " bg-blue-600 hover:bg-blue-700"
                 }`}
         >
             {isMarked && <BookOpenCheck className="h-5 w-5" />}
